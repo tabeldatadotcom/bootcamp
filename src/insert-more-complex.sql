598313c45a71147c34bac996b7351cf76700d4fd
dimMaryanto@win10 <software.dimmaryanto@hotmail.com>
2017-12-04 11:25:14 +0700

    update materi dml dan transaction

---
+++ src/insert-more-complex.sql
@@ -0,0 +1,14 @@
+﻿-- select * from job_history;
+insert into job_history (
+	employee_id, 
+	start_date, 
+	end_date, 
+	job_id, 
+	department_id
+) values (
+	100, 
+	'2011-01-13 12:00:00', 
+	'2017-01-13 08:00:00', 
+	'IT_PROG', 
+	90
+);
---
+++ src/insert-statement.sql
@@ -0,0 +1,9 @@
+﻿-- tambah data ke tabel departments
+insert into departments (
+	department_id, 
+	department_name, 
+	location_id
+) values 
+	(502,'Dev Ops',2700) , 
+	(501,'Database Administrator',2700), 
+	(503,'Jaringan Komputer',2700);
