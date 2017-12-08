acbb3f4fc8e11e0abc88d5a555f32e6193bf375f
dimmaryanto93 <engineer.dimmaryanto93@outlook.com>
2017-12-08 19:02:32 +0700

    update materi typescript classes dan interface

--- intro-ts/main.ts
+++ intro-ts/main.ts
@@ -5,8 +5,12 @@ var mahasiswa = {
 }
 console.log('nama mahasiswa '+ mahasiswa.nama + " alamantnya "+ mahasiswa.alamat);
 
+let namaDepan: string = "Dimas";
+let namaBelakang = "Maryanto";
+
 var mahasiswa = {
-    nama: "Dimas Maryanto",
+    nama: `${namaDepan} 
+            ${namaBelakang}`,
     alamat: "Jl. bukit indah"
 }
 
@@ -14,8 +18,66 @@ let mhs: string = "Dimas Maryanto";
 
 function halo(){
     let mhs: string = "value double";
-    console.log('nama mahasiswa '+ mhs);
+    console.log(`nama dari mahasiswa adalah ${ mahasiswa.nama } `);
 }
 
 console.log('nama mahasiswa '+ mhs);
 halo();
+
+let listNip: Array<number> = [1, 3, 5, 6, 7, 9, 10];
+console.log('index ke 3 adalah '+listNip[3]);
+
+let listData: [number, string, Date] = [1, "Dimas Maryanto", new Date, 2, "Hadi"];
+for( let data in listData){
+    console.log(`index ke ${data} adalah ${listData[data]}`);
+}
+
+let listMhs : Array<{nama, nim}> = [
+    {nama: "Dimas", nim: "1051148"}, 
+    {nama: "adyo", nim: "061234"}
+];
+
+for(let data of listMhs){
+    console.log(data.nama);
+}
+let myData: any = false;
+myData = "asldkjfdsf";
+myData = 10;
+
+interface Mahasiswa{
+    nama: string;
+    nim: string;
+
+    greeting();
+}
+
+class Motor implements Mahasiswa{
+
+    nama: string = "Honda";
+    nim: string = "10511148"
+
+    greeting(){ }
+
+    constructor(nama?: string){
+        this.manufactor = nama;
+    }
+
+    manufactor: string;
+    jumlahRoda : number = 2;
+
+     getJumlahRoda(): number{
+        return this.jumlahRoda;
+    }
+
+    getManufacture(): string{
+        return this.manufactor;
+    }
+
+    toString(): void{
+        console.log(`nama manufacture ${this.getManufacture()} jumlah rodanya ${this.getJumlahRoda()}`);
+    }
+}
+
+let honda: Motor = new Motor();
+honda.toString();
+honda.greeting()
