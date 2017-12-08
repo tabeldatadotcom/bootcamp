5fa5476599edc5c15fe1608d8e903042162a1d27
dimmaryanto93 <engineer.dimmaryanto93@outlook.com>
2017-12-08 14:59:11 +0700

    basic typescript

---
+++ intro-ts/main.ts
@@ -0,0 +1,21 @@
+// in javascript
+var mahasiswa = {
+    nama: "Dimas Maryanto",
+    alamat: "Jl. bukit indah"
+}
+console.log('nama mahasiswa '+ mahasiswa.nama + " alamantnya "+ mahasiswa.alamat);
+
+var mahasiswa = {
+    nama: "Dimas Maryanto",
+    alamat: "Jl. bukit indah"
+}
+
+let mhs: string = "Dimas Maryanto";
+
+function halo(){
+    let mhs: string = "value double";
+    console.log('nama mahasiswa '+ mhs);
+}
+
+console.log('nama mahasiswa '+ mhs);
+halo();
