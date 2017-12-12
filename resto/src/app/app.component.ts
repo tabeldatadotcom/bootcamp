b5d61a329e804d171ad68e1c1c2998e33b8945ff
dimmaryanto93 <engineer.dimmaryanto93@outlook.com>
2017-12-12 14:31:25 +0700

    fundamental angular component & binding data

--- resto/src/app/app.component.ts
+++ resto/src/app/app.component.ts
@@ -2,9 +2,46 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>My favorite super hero is {{ myFavorite }}</h1>
+    <br/>
+    Daftar super hero:
+    <!-- ini komentar -->
+    <ul>
+      <li *ngFor="let hero of superheroes; let i = index">
+        <button (click)="showMeTheValue(i)">
+          {{ hero }}
+        </button>
+      </li>
+    </ul>
+    <label for="namaSuperhero">Nama Super Hero</label>
+    <input id="namaSuperhero" type="text" 
+      name="myfavorite" [(ngModel)]="myFavorite"/>
+    <br/>
+    <button (click)="showTheValue()">Tambah</button><br/>
+  `
 })
 export class AppComponent {
-  title = 'My App';
+
+  showTheValue() {
+    this.superheroes.push(this.myFavorite);
+    // console.log(this.myFavorite);
+  }
+
+  myFavorite = 'Iron Man';
+  superheroes: Array<string> = [
+    'Superman',
+    'Batman',
+    'Wonder Women',
+    'Iron Man'
+  ];
+
+  onChangeText(value) {
+    console.log(value);
+  }
+
+  showMeTheValue(value: number) {
+    this.superheroes.splice(value, 1);
+  }
 }
+
