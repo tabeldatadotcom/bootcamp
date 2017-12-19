29321f140746412633dcd32ca8b353edb5702742
dimmaryanto93 <engineer.dimmaryanto93@outlook.com>
2017-12-19 11:35:00 +0700

    membuat api dengan query param

--- resto-api/src/main/java/com/gmail/maryanto/dimas/resto/restoapi/controller/IndexController.java
+++ resto-api/src/main/java/com/gmail/maryanto/dimas/resto/restoapi/controller/IndexController.java
@@ -8,6 +8,7 @@ package com.gmail.maryanto.dimas.resto.restoapi.controller;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -16,15 +17,27 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class IndexController {
-    
+
     @RequestMapping(
-            path = "/halo", 
+            path = "/halo",
             produces = MediaType.APPLICATION_JSON_VALUE,
             method = RequestMethod.GET)
-    public String haloWorld(){
+    public String haloWorld() {
         return "{ \"nana\" : \"Dimas Maryanto\", "
                 + "\"aplikasi\": \"Aplikasi Web Resto\""
                 + "}";
     }
-    
+
+    @RequestMapping(
+            path = "/hello",
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            method = RequestMethod.GET)
+    public String haloWorld(
+            @RequestParam(name = "nama_lengkap") String nama,
+            @RequestParam(name = "aplikasi")String aplikasi) {
+        return "{ \"nana\" : \"" + nama + "\", "
+                + "\"aplikasi\": \""+aplikasi+"\""
+                + "}";
+    }
+
 }
