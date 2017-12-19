9071d6aa3329ff1ecaca7713b2450a402f39f450
dimmaryanto93 <engineer.dimmaryanto93@outlook.com>
2017-12-19 10:51:54 +0700

    migration from desktop aplikasi to web-mvc

---
+++ resto-api/src/main/java/com/gmail/maryanto/dimas/resto/restoapi/controller/IndexController.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gmail.maryanto.dimas.resto.restoapi.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author dimmaryanto93
+ */
+@RestController
+public class IndexController {
+    
+    @RequestMapping(
+            path = "/halo", 
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            method = RequestMethod.GET)
+    public String haloWorld(){
+        return "{ \"nana\" : \"Dimas Maryanto\", "
+                + "\"aplikasi\": \"Aplikasi Web Resto\""
+                + "}";
+    }
+    
+}
